e73370cd7b90c20dad23ff4012133862c3e49aaa
preethipantangi <pantangisaipreethi@gmail.com>
2023-08-06 15:21:02 -0400

    Leet Code - Problem 875

---
+++ 875/875.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    let k = Infinity;
+    let start = 1; 
+    let end = Math.max(...piles);
+            
+    while (start <= end) {
+        mid = parseInt(start + (end - start)/2);
+        totalHours = 0;
+        for (let i = 0; i < piles.length; i++) {
+            totalHours += Math.ceil(piles[i] / mid);
+        }
+        if(totalHours <= h) {
+            k = Math.min(k, mid);
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return k;
+};
+
+var getMax = function (piles) {
+    let max = -1;
+    for (let i = 0; i < piles.length; i++) {
+        if(piles[i] > max) {
+            max = piles[i];
+        }
+    }
+    return max;
+}
