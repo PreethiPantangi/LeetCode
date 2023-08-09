31d75c4092f7313e7967455f55dfcfdfe52b5d70
preethipantangi <pantangisaipreethi@gmail.com>
2023-08-09 14:45:08 -0400

    Leet Code - Problem 33 Approach 2

--- 33/33_Approach2.js
+++ 33/33_Approach2.js
@@ -3,22 +3,43 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let start = 0; 
-    let end = nums.length - 1;
-    while (start < end) {
-        mid = parseInt(start + (end - start)/2);
-        midEle = nums[mid];
-        lastEle = nums[end];
+    let pivotIndex = getMinElePos(nums);
+    if (target >= nums[0] && target <= nums[pivotIndex-1]) {
+        start = 0;
+        end = pivotIndex - 1;
+    } else {
+        start = pivotIndex;
+        end = nums.length - 1;
+    }
+    while (start <= end) {
+        mid = parseInt(start + (end - start)/2);    
         if (nums[mid] === target) {
-            return mid
-        }
-        if (midEle > lastEle) {
-            start = mid + 1;
+            return mid;
         } else {
-            end = mid;
+            if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
         }
     }
     return -1;
 };
 
-console.log(search([1,2,3,4,5], 4));
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var getMinElePos = function(nums) {
+    let start = 0; 
+    let end = nums.length - 1;
+    while (start < end) {
+        mid = parseInt(start + (end - start)/2);
+        if (nums[mid] > nums[end]) {
+            start = mid + 1;
+        } else {
+            end = mid;
+        }
+    }
+    return start;
+};
