bc47ef88962f4071388246fe680ce505acd7871c
preethipantangi <pantangisaipreethi@gmail.com>
2023-08-09 15:40:06 -0400

    Finding sqrt of a number with precision

---
+++ 69/69_SqrtWithPrecision.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let start = 1;
+    let end = x;
+    ans = 0;
+    while (start <= end) {
+        mid = parseInt(start + (end - start)/2);
+        if (mid*mid === x) {
+            return mid;
+        } else {
+            if (mid*mid > x) {
+                end = mid - 1;
+            } else {
+                ans = mid;
+                start = mid + 1;
+            }
+        }
+    }
+    ans += 0.1;
+    while (ans*ans <= x) {
+        ans += 0.1
+    }
+    return ans;
+};
+
+console.log(mySqrt(2));
