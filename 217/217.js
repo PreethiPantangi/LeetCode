5fa757617d21d2c16a07146c151a408e2d897b19
preethipantangi <pantangisaipreethi@gmail.com>
2023-10-17 11:05:21 -0400

    Leetcode - Problem 217

--- 217/217.js
+++ 217/217.js
@@ -4,17 +4,12 @@
  */
 var containsDuplicate = function(nums) {
     let map = {};
-    let hasDuplicate = false;
     for(let i = 0; i < nums.length; i++) {
         if(nums[i] in map) {
-            map[nums[i]] += 1;
+            return true;
         } else {
             map[nums[i]] = 1;
         }
-        if (map[nums[i]] > 1) {
-            hasDuplicate = true;
-            break;
-        }
     }
-    return hasDuplicate;
+    return false;
 };
