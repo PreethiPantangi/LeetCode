0025f20c58f441aa99f7757c5697a786b60329c6
preethipantangi <pantangisaipreethi@gmail.com>
2023-10-25 18:45:05 -0400

    LeetCode - Problem 219

---
+++ 219/219.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+
+var containsNearbyDuplicate = function(nums, k) {
+    const numIndices = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (numIndices.has(nums[i]) && i - numIndices.get(nums[i]) <= k) {
+            return true;
+        }
+        numIndices.set(nums[i], i)
+    }
+    return false;
+};
