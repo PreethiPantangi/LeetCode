f76a6dbf345e46264a49714e1c8e362539a534a1
preethipantangi <pantangisaipreethi@gmail.com>
2023-11-01 15:23:48 -0400

    LeetCode - Problem 20

--- 20/20.js
+++ 20/20.js
@@ -3,34 +3,26 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let stack = [];
     if(s.length === 1) {
         return false; 
     } else {
-        for(let i = 0; i < s.length; i++) {
-            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+        let stack = [];
+        for (let i = 0; i < s.length; i++) {
+            if (['(', '{', '['].includes(s[i])) {
                 stack.push(s[i]);
-            } 
-            if (stack.length == 0)
-                return false;
-            switch(s[i]) {
-                case ')':
-                    check = stack.pop();
-                    if (check == '{' || check == '[')
-                        return false;
-                    break;
-                case '}':
-                    check = stack.pop();
-                    if (check == '(' || check == '[')
-                        return false;
-                    break;
-                case ']':
-                    check = stack.pop();
-                    if (check == '(' || check == '{')
-                        return false;
-                    break;
+            } else {
+                let bracket = stack.pop();
+                if(
+                    (s[i] === ')' && bracket !== '(') || 
+                    (s[i] === ']' && bracket !== '[') ||
+                    (s[i] === '}' && bracket !== '{')
+                ) {
+                    return false;
+                }
             }
         }
         return stack.length === 0;
     }
 };
+
+console.log(isValid('()[]{}(([}]))'));
