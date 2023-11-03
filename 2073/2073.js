90d935ff4d8f8910450390f6d3e7ee27a8fd205c
preethipantangi <pantangisaipreethi@gmail.com>
2023-11-02 21:17:41 -0400

    LeetCode - Problem 2073

---
+++ 2073/2073.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function(tickets, k) {
+    let res = tickets[k];
+    for (let i = 0; i < k; i++) {
+        res += Math.min(tickets[k], tickets[i]);
+    }
+    for (let i = k + 1; i < tickets.length; i++) {
+        res += Math.min(tickets[k] - 1, tickets[i]);
+    }
+
+    return res;
+};
