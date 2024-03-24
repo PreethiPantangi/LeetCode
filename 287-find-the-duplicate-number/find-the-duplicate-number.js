b6aaa6e89c5cdbd7e04b55ce8b05100dd7a6ae3f
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-03-23 20:32:41 -0400

    Time: 97 ms (37.67%) | Memory: 64.1 MB (35.99%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -6,7 +6,6 @@ var findDuplicate = function(nums) {
     let map = {};
     for(let i = 0; i < nums.length; i++) {
         if(map[nums[i]]) {
-            // map[nums[i]] += 1;
             return nums[i];
         } else {
             map[nums[i]] = 1;
