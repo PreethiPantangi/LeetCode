6caa4c0ddf0de8314282a37bbedf2faacb96d09d
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-03-24 22:33:42 -0400

    Time: 84 ms (65.99%) | Memory: 59.2 MB (63.01%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.js
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.js
@@ -3,10 +3,9 @@
  * @return {number[]}
  */
 var findDuplicates = function(nums) {
-    let len = nums.length;
-    let arr = new Array(len+1).fill(0);
+    let arr = new Array(nums.length+1).fill(0);
     let res= [];
-    for(let i = 0; i < len; i++) {
+    for(let i = 0; i < nums.length; i++) {
         arr[nums[i]] += 1;
     }
     for(let i = 0; i < arr.length; i++) {
