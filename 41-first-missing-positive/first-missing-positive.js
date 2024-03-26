6c8799952e74ceafa18044206ce809245eb92deb
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-03-25 21:16:11 -0400

    Time: 80 ms (48.12%) | Memory: 65 MB (29.69%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.js
+++ 41-first-missing-positive/first-missing-positive.js
@@ -3,32 +3,12 @@
  * @return {number}
  */
 var firstMissingPositive = function(nums) {
-    if(nums.length === 1 ) {
-        if (nums[0] === 1) {
-            return 2;
-        } else if (nums[0] === 0) {
-            return 1;
-        } else if (nums[0] > 1 || nums[0] < 0) {
-            return 1;
+    const numSet = new Set(nums);
+    for (let i = 1; i <= nums.length + 1; i++) {
+        if (!numSet.has(i)) {
+            return i;
         }
-    } else {
-        let max = Math.max(...nums);
-        if(max > 0) {
-            let map = new Map();
-            for(let i = 0; i < nums.length; i++) {
-                if(map.get(nums[i])) {
-                    map.set(nums[i], map.get(nums[i]) + 1);
-                } else {
-                    map.set(nums[i], 1);
-                }
-            }
-            for(let i = 1; i <= max+1; i++) {
-                if(!map.get(i)) {
-                    return i;
-                }
-            }
-        } else {
-            return 1;
-        }        
     }
+    
+    return 1;
 };
