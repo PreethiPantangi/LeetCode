8bde1c69569348b425e7a8cdb7898e1f7214b9f6
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-03-31 20:38:38 -0400

    Time: 44 ms (89.86%) | Memory: 49 MB (37.17%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,10 +3,5 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    s = s.split(" ");
-    for(let i = s.length-1; i >= 0; i--) {
-        if(s[i] !== "") {
-            return s[i].length;
-        }
-    }
+    return s.trim().split(" ").pop().length;
 };
