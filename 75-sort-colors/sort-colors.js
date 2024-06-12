59ef32355a0299a2e8eaaa86965ff7cc2318e9c6
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-06-12 11:43:55 -0400

    Time: 60 ms (20.41%) | Memory: 49.9 MB (8.22%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -3,13 +3,46 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    for(let i = 0; i < nums.length; i++) {
-        let minIndex = i;
-        for(let j = i+1; j < nums.length; j++) {
-            if(nums[j] < nums[minIndex]) {
-                [nums[j], nums[minIndex]] = [nums[minIndex], nums[j]];
+    const merge = (nums, start, mid, end) => {
+        let leftArrLen = mid - start + 1;
+        let rightArrLen = end - mid;
+        let leftArr = new Array(leftArrLen);
+        let rightArr = new Array(rightArrLen);
+        for(let i = 0; i < leftArrLen; i++) {
+            leftArr[i] = nums[start + i];
+        }
+        for(let i = 0; i < rightArrLen; i++) {
+            rightArr[i] = nums[mid + i + 1];
+        }
+        let i = 0, j = 0, k = start;
+        while(i < leftArrLen && j < rightArrLen) {
+            if(leftArr[i] <= rightArr[j]) {
+                nums[k] = leftArr[i];
+                i += 1;
+            } else {
+                nums[k] = rightArr[j];
+                j += 1;
             }
+            k += 1;
+        }
+        while(i < leftArrLen) {
+            nums[k] = leftArr[i];
+            i += 1;
+            k += 1;
+        }
+        while(j < rightArrLen) {
+            nums[k] = rightArr[j];
+            j += 1;
+            k += 1;
         }
     }
+    const mergeSort = (nums, start, end) => {
+        if (start >= end) return;
+        let mid = parseInt((start + end)/2);
+        mergeSort(nums, start, mid);
+        mergeSort(nums, mid+1, end);
+        merge(nums, start, mid, end);
+    };
+    mergeSort(nums, 0, nums.length-1);
     return nums;
 };
