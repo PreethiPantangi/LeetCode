cd7d14c1ebc7f93566389541aaf42fce579c47b9
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-06-30 20:46:26 -0400

    Time: 46 ms (88.27%) | Memory: 49.1 MB (38.27%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.js
+++ 1293-three-consecutive-odds/three-consecutive-odds.js
@@ -3,14 +3,12 @@
  * @return {boolean}
  */
 var threeConsecutiveOdds = function(arr) {
-    let i = 0, j = i + 1, k = j + 1;
-    while(k < arr.length) {
-        if((arr[i] % 2 !== 0) && (arr[j] % 2 !== 0) && (arr[k] % 2 !== 0)) {
-            return true;
-        }
+    let oddCounter = 0;
+    let i = 0;
+    while(i < arr.length) {
+        oddCounter = (arr[i] % 2 !== 0) ? oddCounter + 1 : 0;
+        if(oddCounter === 3) return true;
         i += 1;
-        j += 1;
-        k += 1;
     }
     return false;
 };
