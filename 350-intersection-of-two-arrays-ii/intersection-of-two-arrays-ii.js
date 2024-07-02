11eb06579c502bee33e345b55d8f79b493fd1147
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-07-01 20:10:20 -0400

    Time: 51 ms (85.32%) | Memory: 49.2 MB (94.79%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    let res = [];
+    if(nums1.length > nums2.length) {
+        [nums1, nums2] = [nums2, nums1];
+    }
+    nums1.forEach((num) => {
+        if(nums2.includes(num)) {
+            res.push(num);
+            nums2.splice(nums2.indexOf(num), 1)
+        }
+    });
+    return res;
+};
