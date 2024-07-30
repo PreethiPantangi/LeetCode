8796bfb4e7e973bf94aadaa8f5ca29727b05ce2a
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-07-29 22:14:41 -0400

    Time: 63 ms (13.58%) | Memory: 48.5 MB (93.21%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.js
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.js
@@ -4,17 +4,13 @@
  * @return {number}
  */
 var minBitFlips = function(start, goal) {
-    let bin1 = start.toString(2);
-    let bin2 = goal.toString(2);
-    let maxLen = Math.max(bin1.length, bin2.length);
-    bin1 = bin1.padStart(maxLen, '0');
-    bin2 = bin2.padStart(maxLen, '0');
+    let xor = start ^ goal;
     let res = 0;
-    for(let i = maxLen - 1; i >= 0 ; i--) {
-        console.log(bin1[i], bin2[i]);
-        if(bin1[i] !== bin2[i]) {
-            res += 1;
-        }
+    while(xor > 0) {
+        // Gives the LSB of the binary rep of the num
+        res += (xor & 1);
+        // Remove the LSB to get the next one
+        xor >>= 1;
     }
     return res;
 };
