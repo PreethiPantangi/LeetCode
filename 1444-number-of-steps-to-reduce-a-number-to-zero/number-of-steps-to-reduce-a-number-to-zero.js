3093983be5d2ce50cd9f68f54805ecaa0e36de95
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-07-29 21:56:34 -0400

    Time: 49 ms (78.61%) | Memory: 49 MB (30.51%) - LeetSync

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let res = 0;
+    while(num !== 0) {
+        if(num % 2 === 0) {
+            num /= 2;
+        } else {
+            num -= 1;
+        }
+        res += 1;
+    }
+    return res;
+};
