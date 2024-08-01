eb2252844af1a0cf10211d084e228bda153d71c8
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-07-31 20:15:44 -0400

    Time: 54 ms (73.75%) | Memory: 50.6 MB (81.38%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.js
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 var countSeniors = function(details) {
-    let res = 0;
     return (details.filter((detail) => {
         return parseInt(detail.slice(11,13)) > 60
     })).length;
