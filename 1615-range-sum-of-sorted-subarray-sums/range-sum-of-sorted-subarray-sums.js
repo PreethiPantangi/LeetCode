f6f7e6813dce97a65d59053f840b15b78eb8f831
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-08-03 21:01:47 -0400

    Time: 276 ms (84.62%) | Memory: 81.5 MB (61.54%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.js
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.js
@@ -6,17 +6,25 @@
  * @return {number}
  */
 var rangeSum = function(nums, n, left, right) {
+    let prefixSum = new Array(n + 1).fill(0);
+    for (let i = 0; i < n; i++) {
+        prefixSum[i + 1] = prefixSum[i] + nums[i];
+    }
+
     let values = [];
     for (let start = 0; start < n; start++) {
         for (let end = start; end < n; end++) {
-            let sum = 0;
-            values.push((nums.slice(start, end + 1)).reduce((curr, acc) => curr + acc, sum));
+            values.push(prefixSum[end + 1] - prefixSum[start]);
         }
     }
-    values.sort((a,b) => a - b);
+
+    values.sort((a, b) => a - b);
+    
     let res = 0;
-    for(let i = left-1; i < right; i++) {
+    for (let i = left - 1; i < right; i++) {
         res += values[i];
+        res %= (10 ** 9 + 7);
     }
-    return res % ((10 ** 9) + 7);
+
+    return res;
 };
