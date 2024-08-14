f096a7ed33f32a44b25ec26bf09260009b0dd4d2
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-08-13 23:18:16 -0400

    Time: 61 ms (80.51%) | Memory: 51.9 MB (57.99%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -3,11 +3,5 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    let n = nums.length;
-    let res = new Array(n * 2).fill(0);
-    for(let i = 0; i < n; i++) {
-        res[i] = nums[i];
-        res[i+n] = nums[i];
-    }
-    return res;
+    return nums.concat(nums);
 };
