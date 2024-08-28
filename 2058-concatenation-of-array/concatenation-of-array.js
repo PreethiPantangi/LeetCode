d2788869cf69ae52155aa53a0646176e5645825a
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-08-27 23:05:03 -0400

    Time: 74 ms (13.70%) | Memory: 53.1 MB (5.66%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -3,5 +3,10 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    return nums.concat(nums);
+    let ans = [];
+    let n = nums.length;
+    for(let i = 0; i < nums.length; i++) {
+        [ans[i], ans[i+n]] = [nums[i], nums[i]];
+    }
+    return ans;
 };
