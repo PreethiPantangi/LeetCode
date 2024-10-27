72fcf6e44e8e742ea5137e0fbedd974cc4151e89
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-10-27 00:40:45 -0400

    Time: 83 ms (62.65%) | Memory: 70.9 MB (64.81%) - LeetSync

--- 948-sort-an-array/sort-an-array.js
+++ 948-sort-an-array/sort-an-array.js
@@ -3,41 +3,59 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-    const mergeSort = (arr, left, right) => {
-        if (left >= right) return;
-        
-        const mid = Math.floor((left + right) / 2);
-        
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
-    };
-
-    const merge = (arr, left, mid, right) => {
-        const temp = [];
-        let i = left, j = mid + 1;
-        
-        while (i <= mid && j <= right) {
-            if (arr[i] <= arr[j]) {
-                temp.push(arr[i++]);
-            } else {
-                temp.push(arr[j++]);
-            }
-        }
-        
-        while (i <= mid) {
-            temp.push(arr[i++]);
-        }
-        
-        while (j <= right) {
-            temp.push(arr[j++]);
-        }
-        
-        for (let k = 0; k < temp.length; k++) {
-            arr[left + k] = temp[k];
-        }
-    };
-
     mergeSort(nums, 0, nums.length - 1);
-    return nums;
+    return  nums;
 };
+
+function mergeSort(nums, left, right) {
+    if(left >= right) {
+        return;
+    }
+    const mid = Math.floor(left + (right - left)/2);
+    mergeSort(nums, left, mid);
+    mergeSort(nums, mid+1, right);
+    merge(nums, left, mid, right);
+}
+
+function merge(nums, left, mid, right) {
+    let leftLen = mid - left + 1;
+    let rightLen = right - mid;
+    let leftArray = new Array(leftLen);
+    let rightArray = new Array(rightLen);
+
+    for(let i = 0; i < leftLen; i++) {
+        leftArray[i] = nums[left + i];
+    }
+
+    for(let i = 0; i < rightLen; i++) {
+        rightArray[i] = nums[mid + 1 + i];
+    }
+
+    let i = 0; 
+    let j = 0;
+    let k = left;
+
+    while(i < leftLen && j < rightLen) {
+        if(leftArray[i] <= rightArray[j]) {
+            nums[k] = leftArray[i];
+            i += 1;
+        } else {
+            nums[k] = rightArray[j];
+            j += 1;
+        }
+        k += 1;
+    }
+
+    while (i < leftLen) {
+        nums[k] = leftArray[i];
+        k += 1;
+        i += 1;
+    }
+
+    while (j < rightLen) {
+        nums[k] = rightArray[j];
+        k += 1;
+        j += 1;
+    }
+
+}
