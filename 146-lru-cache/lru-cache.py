34da36273e3f3fbdd71b14425964e746140269ca
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-11-12 20:57:39 -0500

    Time: 137 ms (62.61%) | Memory: 77.8 MB (55.04%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,30 +1,55 @@
 from collections import deque
+
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value 
+        self.next = None
+        self.prev = None 
+        
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.queue = deque([])
         self.cache = {}
+        self.head = Node(-1, -1)
+        self.tail = Node(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head 
+
+    def addNode(self, node):
+        current_node = self.tail.prev 
+        current_node.next = node
+        node.prev = current_node
+        node.next = self.tail
+        self.tail.prev = node
+    
+    def removeNode(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
-        value = self.cache[key]
-        self.queue.remove(key)
-        self.queue.appendleft(key)
-        return value
+        node = self.cache[key]
+        self.removeNode(node)
+        self.addNode(node)
+        return node.value
         
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.cache[key] = value
-            self.queue.remove(key)
-            self.queue.appendleft(key)
-        else:
-            if len(self.cache) >= self.capacity:
-                lru_key = self.queue.pop()
-                del self.cache[lru_key]
-            self.cache[key] = value
-            self.queue.appendleft(key)
+            node = self.cache[key]
+            self.removeNode(node)
+        
+        new_node = Node(key, value)
+        self.cache[key] = new_node
+        self.addNode(new_node)
+
+        if len(self.cache) > self.capacity:
+            delete_node = self.head.next
+            self.removeNode(delete_node)
+            del self.cache[delete_node.key]
 
 
 
