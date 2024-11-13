fae5dcd08a3ec33156268c6ea9deea556227e439
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-11-12 21:16:02 -0500

    Time: 163 ms (33.17%) | Memory: 77.3 MB (90.13%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,33 +1,30 @@
-from collections import deque
-
 class Node:
     def __init__(self, key, value):
         self.key = key
         self.value = value 
         self.next = None
         self.prev = None 
-        
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = {}
-        self.head = Node(-1, -1)
-        self.tail = Node(-1, -1)
+        self.head = Node(-1, 1)
+        self.tail = Node(-1, 1) 
         self.head.next = self.tail
-        self.tail.prev = self.head 
+        self.tail.prev = self.head
 
     def addNode(self, node):
         current_node = self.tail.prev 
-        current_node.next = node
-        node.prev = current_node
+        current_node.next = node 
+        node.prev = current_node 
         node.next = self.tail
         self.tail.prev = node
-    
+
     def removeNode(self, node):
         node.prev.next = node.next
-        node.next.prev = node.prev
-        
+        node.next.prev = node.prev 
 
     def get(self, key: int) -> int:
         if key not in self.cache:
@@ -35,13 +32,12 @@ class LRUCache:
         node = self.cache[key]
         self.removeNode(node)
         self.addNode(node)
-        return node.value
+        return node.value        
         
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             node = self.cache[key]
             self.removeNode(node)
-        
         new_node = Node(key, value)
         self.cache[key] = new_node
         self.addNode(new_node)
@@ -50,6 +46,7 @@ class LRUCache:
             delete_node = self.head.next
             self.removeNode(delete_node)
             del self.cache[delete_node.key]
+
 
 
 
