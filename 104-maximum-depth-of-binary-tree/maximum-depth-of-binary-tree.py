ccf72b50a168bfd00d073cbe4670578de1efcca7
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-11-16 17:22:54 -0500

    Time: 2 ms (38.12%) | Memory: 17.9 MB (36.67%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -4,8 +4,28 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+from collections import deque
+
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        result = 0
         if root is None:
-            return 0
-        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+            return result
+        
+        queue = deque([root])
+
+        while queue:
+            queue_size = len(queue)
+            level = []
+
+            for _ in range(queue_size):
+                node = queue.popleft()
+                level.append(node.val)
+
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
+            result += 1 if len(level) > 0 else 0
+        return result
