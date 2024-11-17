fb138bf59cabb2bad0533965219bff88ad40b354
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-11-16 23:52:24 -0500

    Time: 12 ms (57.79%) | Memory: 19.5 MB (88.34%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,7 +2,6 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         _map = {}
         for word in strs:
-            sorted_word = word
             sorted_word = ''.join(sorted(word))
             if sorted_word in _map:
                 _map[sorted_word].append(word)
