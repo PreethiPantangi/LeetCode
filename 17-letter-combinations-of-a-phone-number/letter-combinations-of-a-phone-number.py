799e4aafc5ef39123f756030713100d47a965975
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-11-18 16:58:42 -0500

    Time: 0 ms (100.00%) | Memory: 16.7 MB (9.42%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,28 +1,30 @@
 class Solution:
-    def generateCombinations(self, arr1, arr2):
-        result = []
-        for i in range(len(arr1)):
-            for j in range(len(arr2)):
-                result.append(arr1[i] + arr2[j])
-        return result
-
     def letterCombinations(self, digits: str) -> List[str]:
         if len(digits) == 0:
             return []
+        
         mapping = {
-            "2": ["a", 'b', 'c'],
-            "3": ["d", "e", "f"],
-            "4": ["g", "h", "i"],
-            "5": ["j", "k", "l"],
-            "6": ["m", "n", "o"],
-            "7": ["p", "q", "r", "s"],
-            "8": ["t", "u", "v"],
-            "9": ["w", "x", "y", "z"]
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
         }
 
-        result = mapping[digits[0]]
-
-        for i in range(1, len(digits)):
-            result = self.generateCombinations(result, mapping[digits[i]])
-        return result
+        def getCombinations(index, s):
+            if len(s) == len(digits):
+                result.append("".join(s))
+                return
+            
+            letters = mapping[digits[index]]
+            for letter in letters:
+                s.append(letter)
+                getCombinations(index+1, s)
+                s.pop()
         
+        result = []
+        getCombinations(0, [])
+        return result
