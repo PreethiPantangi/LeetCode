ad7588e75cfe6e28bcb45b3318bc1db85bdaac01
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-11-18 22:28:27 -0500

    Time: 41 ms (22.21%) | Memory: 16.5 MB (65.49%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -6,9 +6,12 @@ class Solution:
         else:
             sign = 1
         
-        rev_num = 0
         x = abs(x)
+        rev_num = 0
         while x:
-            rev_num = (rev_num * 10) + (x % 10)
+            rev_num = (rev_num * 10) + x % 10
             x = x // 10
-        return 0 if (rev_num >= pow(2,31) - 1 or rev_num <= -pow(2,31)) else rev_num * sign
+
+        MAX = pow(2,31) - 1
+        MIN  = -pow(2,31)
+        return 0 if rev_num >= MAX or rev_num <= MIN else rev_num * sign
