365995196e4c9d615ec581e0347b05cf6ce7a943
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-12-02 20:58:01 -0500

    Time: 76 ms (80.00%) | Memory: 89.1 MB (59.68%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.js
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.js
@@ -6,17 +6,13 @@
 var addSpaces = function(s, spaces) {
     let strLen = s.length;
     let spacesLen = spaces.length;
+    let prevIndex = 0;
     let result = "";
     for(let i = 0; i < spacesLen; i++) {
-        if(i - 1 >= 0) {
-            result += s.substr(spaces[i-1], spaces[i]-spaces[i-1]);
-        } else  {
-            result += s.substr(0, spaces[i]);
-        }
-        if(spacesLen !== i) {
-            result += " ";
-        }
+        result += s.slice(prevIndex, spaces[i]);
+        result += " ";
+        prevIndex = spaces[i];
     }
-    result += s.substr(spaces[spacesLen-1], strLen-spaces[spacesLen-1]);
+    result += s.slice(prevIndex);
     return result;
 };
