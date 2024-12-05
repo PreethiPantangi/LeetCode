224b8500ec9e0cac4b5ae70448caa79402aab1c5
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-12-04 19:04:15 -0500

    Time: 509 ms (69.11%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,11 +1,6 @@
 # Write your MySQL query statement below
 
-with result as (
-    select * from Activity 
-    where activity_date between '2019-06-28' and '2019-07-27'
-    group by user_id, activity_date
-)
-
-SELECT result.activity_date AS day, count(*) as active_users 
-FROM result
-GROUP BY result.activity_date
+SELECT activity_date AS day, count(DISTINCT user_id) AS active_users
+FROM Activity 
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY activity_date
