490378edd8074bfde944e0ade37567ed549acf36
Preethi Pantangi <pantangisaipreethi@gmail.com>
2024-12-12 16:01:42 -0500

    Time: 1367 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,17 +1,16 @@
 # Write your MySQL query statement below
 
-WITH distinct_lat_long as (
-    SELECT lat, lon, count(*)
+
+SELECT ROUND(sum(i.tiv_2016), 2) as tiv_2016 
+FROM Insurance i
+WHERE (i.lat, i.lon) IN (
+    SELECT lat, lon
     FROM Insurance  
     GROUP BY lat, lon
     HAVING count(*) = 1
-), same_tiv as (
+) AND i.tiv_2015 IN (
     SELECT tiv_2015
     FROM Insurance  
     GROUP BY tiv_2015
     HAVING count(*) > 1
 )
-
-SELECT ROUND(sum(i.tiv_2016), 2) as tiv_2016 FROM Insurance i
-join distinct_lat_long dll on dll.lat = i.lat and dll.lon = i.lon
-join same_tiv st on st.tiv_2015 = i.tiv_2015
