f7a89851480c6d08114d4f2aaa55b6993be9633b
Preethi Pantangi <pantangisaipreethi@gmail.com>
2025-05-04 17:49:21 -0400

    Time: 3 ms (37.11%) | Memory: 56.6 MB (33.33%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -4,15 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let i = 0; 
-    let j = numbers.length - 1;
-    while(i < j) {
-        if(numbers[i] + numbers[j] === target) {
-            return [i+1, j+1];
-        } else if (numbers[i] + numbers[j] > target) {
-            j -= 1;
-        } else {
-            i += 1;
+    let map = {};
+    for(let i = 0; i < numbers.length; i++) {
+        let complement = target - numbers[i];
+        if(map.hasOwnProperty(complement)) {
+            return [map[complement]+1, i+1];
         }
+        map[numbers[i]] = i;
     }
 };
