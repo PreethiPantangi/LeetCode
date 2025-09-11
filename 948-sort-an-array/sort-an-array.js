c1430afa14b464a4f8eca4b2459515998b5de8fa
Preethi Pantangi <pantangisaipreethi@gmail.com>
2025-09-10 23:41:50 -0400

    Time: 67 ms (84.56%) | Memory: 75.2 MB (85.24%) - LeetSync

--- 948-sort-an-array/sort-an-array.js
+++ 948-sort-an-array/sort-an-array.js
@@ -3,59 +3,44 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-    mergeSort(nums, 0, nums.length - 1);
-    return  nums;
+    mergeSort(nums, 0, nums.length-1);
+    return nums;
 };
 
-function mergeSort(nums, left, right) {
-    if(left >= right) {
-        return;
-    }
-    const mid = Math.floor(left + (right - left)/2);
-    mergeSort(nums, left, mid);
-    mergeSort(nums, mid+1, right);
-    merge(nums, left, mid, right);
+var mergeSort = function(nums, low, high) {
+    if(low >= high) return;
+    let mid = Math.floor((low + high)/2);
+    mergeSort(nums, low, mid);
+    mergeSort(nums, mid+1, high);
+    merge(nums, low, mid, high);
 }
 
-function merge(nums, left, mid, right) {
-    let leftLen = mid - left + 1;
-    let rightLen = right - mid;
-    let leftArray = new Array(leftLen);
-    let rightArray = new Array(rightLen);
+var merge = function(nums, low, mid, high) {
+    let temp = [];
+    let i = low;
+    let j = mid+1;
 
-    for(let i = 0; i < leftLen; i++) {
-        leftArray[i] = nums[left + i];
-    }
-
-    for(let i = 0; i < rightLen; i++) {
-        rightArray[i] = nums[mid + 1 + i];
-    }
-
-    let i = 0; 
-    let j = 0;
-    let k = left;
-
-    while(i < leftLen && j < rightLen) {
-        if(leftArray[i] <= rightArray[j]) {
-            nums[k] = leftArray[i];
-            i += 1;
+    while(i <= mid && j <= high) {
+        if(nums[i] <= nums[j]) {
+            temp.push(nums[i]);
+            i++;
         } else {
-            nums[k] = rightArray[j];
-            j += 1;
+            temp.push(nums[j]);
+            j++;
         }
-        k += 1;
     }
 
-    while (i < leftLen) {
-        nums[k] = leftArray[i];
-        k += 1;
-        i += 1;
+    while(i <= mid) {
+        temp.push(nums[i]);
+        i++;
     }
 
-    while (j < rightLen) {
-        nums[k] = rightArray[j];
-        k += 1;
-        j += 1;
+    while(j <= high) {
+        temp.push(nums[j]);
+        j++;
     }
 
+    for(let i = low; i <= high; i++) {
+        nums[i] = temp[i-low];
+    }
 }
