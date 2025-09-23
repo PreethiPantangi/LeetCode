b65746460a03cfc3aed2e65f6d7a438fc5461005
Preethi Pantangi <pantangisaipreethi@gmail.com>
2025-09-22 22:38:56 -0400

    Time: 7 ms (100.00%) | Memory: 49.2 MB (95.31%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,16 +1,16 @@
 class NumArray {
 
-    private int[] numbers;
+    private int[] precomputed;
     public NumArray(int[] nums) {
-        this.numbers = nums;
+        int n = nums.length;
+        precomputed = new int[n+1];
+        for(int i = 0; i < nums.length; i++) {
+            precomputed[i+1] = precomputed[i] + nums[i];
+        }
     }
     
     public int sumRange(int left, int right) {
-        int sum = 0;
-        for(int i = left; i <= right; i++) {
-            sum += this.numbers[i];
-        }
-        return sum;
+        return precomputed[right+1] - precomputed[left];
     }
 }
 
