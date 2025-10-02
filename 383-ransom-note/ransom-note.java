a70efc4e32b58cedb184201e92bb6291a75a9b63
Preethi Pantangi <pantangisaipreethi@gmail.com>
2025-10-01 22:03:39 -0400

    Time: 1 ms (99.20%) | Memory: 44.7 MB (73.16%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] magazineCount = new int[26];
+        for(int i = 0; i < magazine.length(); i++) {
+            magazineCount[magazine.charAt(i) - 'a'] += 1;
+        }
+        for(int i = 0; i < ransomNote.length(); i++) {
+            if(magazineCount[ransomNote.charAt(i) - 'a'] == 0) return false;
+            magazineCount[ransomNote.charAt(i) - 'a'] -= 1;
+        }
+        return true;
+    }
+}
