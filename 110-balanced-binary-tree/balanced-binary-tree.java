48436e2a7d0428dcaf64d1e66e0d222ecca036d0
Preethi Pantangi <pantangisaipreethi@gmail.com>
2026-06-30 11:41:28 -0700

    Time: 0 ms (100.00%) | Memory: 45.6 MB (59.11%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -15,14 +15,17 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root == null) return true;
-        if(Math.abs(height(root.left) - height(root.right)) > 1) return false;
-        return isBalanced(root.left) && isBalanced(root.right);
+        return height(root) != -1;
     }
 
     public int height(TreeNode node) {
         if(node == null) return 0;
-        return 1 + Math.max(height(node.left), height(node.right));
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        if(leftHeight == -1 || rightHeight == -1) return -1;
+        if(Math.abs(leftHeight - rightHeight) > 1) return -1;
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 
 }
